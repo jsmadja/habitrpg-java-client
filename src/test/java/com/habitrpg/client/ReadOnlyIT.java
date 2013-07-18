c23570c9267b8b3b01473ae16ca9928ba02fd70d
Julien Smadja <julien.smadja@gmail.com>
2013-07-18 18:47:15 +0200

    Rename Integration tests and use api-key and api-user to authenticate ReadOnlyIT

---
+++ src/test/java/com/habitrpg/client/ReadOnlyIT.java
@@ -0,0 +1,28 @@
+package com.habitrpg.client;
+
+import com.habitrpg.client.resource.Task;
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collection;
+
+public class ReadOnlyIT {
+
+    @Test
+    public void test() throws MalformedURLException, ResourceNotFoundException {
+        String apiUser = System.getProperty("test.api.user");
+        String apiKey = System.getProperty("test.api.key");
+        if(apiUser == null || apiKey == null) {
+            System.err.println("You should pass -Dtest.api.user=\"<your-api-user>\" -Dtest.api.key=\"<your-api-key>\" to validate client compatibility");
+            return;
+        }
+        HabitRpgClient client = new HabitRpgClient(new AuthenticationInformations(apiUser, apiKey));
+        client.getUser();
+        client.getStatus();
+        for (Task task : client.getTasks()) {
+            client.getTask(task.getId());
+        }
+    }
+
+}
